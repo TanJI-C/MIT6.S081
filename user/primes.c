8ed6aa2216ac7ada9b20a360d913321dbb929e7b
ubuntu <ubuntu@localhost.localdomain>
2023-03-08 20:51:00 +0800

    ac: find

--- user/primes.c
+++ user/primes.c
@@ -60,37 +60,4 @@ main(int argc, char *argv[])
     // wait(0);
     exit(0);
     
-    // int pid;
-    // if((pid = fork1()) != 0){
-    //   close(1);
-    //   dup(p1[1]);
-    //   close(p1[0]);
-    //   close(p1[1]);
-    //   write(1, "a", 1);
-    //   close(1);
-    //   wait(0);
-
-    //   close(0);
-    //   dup(p2[0]);
-    //   close(p2[0]);
-    //   close(p2[1]);
-    //   read(0, ch, 1);
-    //   fprintf(2, "%d: received pong\n", getpid());
-    //   exit(0);
-    // }
-    // else {
-    //   close(0);
-    //   dup(p1[0]);
-    //   close(p1[0]);
-    //   close(p1[1]);
-    //   read(0, ch, 1);
-    //   fprintf(2, "%d: received ping\n", getpid());
-
-    //   close(1);
-    //   dup(p2[1]);
-    //   close(p2[0]);
-    //   close(p2[1]);
-    //   write(1, "b", 1);
-    //   exit(0);
-    // }
 }
